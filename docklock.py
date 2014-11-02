170ef4d8329340236f2c97066fc2e06cff078f59
rarchk <tau.ronak@gmail.com>
2014-11-02 20:03:49 +0530

    Added Security functions

--- docklock.py
+++ docklock.py
@@ -3,7 +3,27 @@
 # 			Security beyond Default				 #
 # 
 #Authors : Ronak Kogta, Shashank Sahni, Gaurav Rajput#
-# 
-# gpg -c filename 
+#  Following package uses gpg symmetric encryption alogrithm to encrypt your container images
 
-# Symmetric encryption or Asymmetric encryption
+#!/usr/bin/env python
+import os,sys; 
+
+class docklock:
+	def encrypt(self,SourceF,passpharase):
+		 os.system("echo "+passpharase+"|gpg --passphrase-fd 0  -c "+SourceF);
+	
+	def decrypt(self,CypherF,SourceF,passphrase):
+		 #os.system("echo "+passphrase+"|gpg --passphrase-fd 0  -d "+CypherF+">"+SourceF);
+		 os.system("echo "+passphrase+"|gpg --passphrase-fd 0  -d "+CypherF);
+
+if __name__ == '__main__':
+	if len(sys.argv) < 4:
+		print 'Usage: docklock [SourceF] [action] [passphrase]'
+		sys.exit(1);
+	
+	engine=docklock();
+	if (sys.argv[2] == 'encrypt'):
+		engine.encrypt(sys.argv[1],sys.argv[3]);
+	elif (sys.argv[2] == 'decrypt'):
+		engine.decrypt(sys.argv[1],str(sys.argv[1].split('.')[:-1]),sys.argv[3]);
+		
